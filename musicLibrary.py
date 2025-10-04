6d2e10e1f3c756d6da4c3f4ee6eb66b69e1ce1d3
Ankit Saini <ankitsaini24082002@gmail.com>
2025-10-04 18:14:08 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "O-shera": "https://youtu.be/hXMjKVKO18g?si=T8r910NoBvBEPQ0h",
+    "kar har maidaan fateh": "https://youtu.be/9iIX4PBplAY?si=kxKphacQie85riHW",
+    "bandeya rey bandeya": "https://youtu.be/Wj8C_bpnkTY?si=C848-FZauAGhxwbp",
+    "parinda": "https://youtu.be/1JPlmLKsrB4?si=ro_HGANAtlGpT-Pz"
+}
